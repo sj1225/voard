67a332379746d54af576a111abf997ba275de48b
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2023-03-04 23:55:58 +0900

    디버그 코드 삭제
    console.log 구문 삭제

--- src/components/layouts/LayoutDefault.vue
+++ src/components/layouts/LayoutDefault.vue
@@ -10,8 +10,6 @@
 import Header from '@/components/layouts/Header.vue'
 import Footer from '@/components/layouts/Footer.vue'
 
-console.log("LayoutDefault.vue");
-
 export default {
     components: {
         Header,
