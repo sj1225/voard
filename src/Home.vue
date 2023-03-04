8425375da8fe7406abe82441ea092840cb7bebb1
m1ngmy0ng <49314605+sj1225@users.noreply.github.com>
2023-03-05 03:14:49 +0900

    메인화면 디자인 개선

--- src/Home.vue
+++ src/Home.vue
@@ -1,11 +1,59 @@
 <template>
-    <h1>Main</h1>
+  <div>
+    <v-card>
+    <v-toolbar flat>
+      <v-toolbar-title>전체 게시글</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-card-text class="grey lighten-4">
+        <v-card>
+
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      hide-default-footer
+      
+    ></v-data-table>
+    <div class="text-center">
+    <v-pagination
+      v-model="page"
+      :length="4"
+      circle
+    ></v-pagination>
+  </div>
+  </v-card>
+    </v-card-text>
+    
+  </v-card>
+
+  </div>
+  
 </template>
 
 <script>
-console.log("Home.vue");
-
-export default {
-    name: 'home',
-}
+  export default {
+    data () {
+      return {
+        page: 1,
+        headers: [
+          {
+            text: '번호',
+            align: 'start',
+            value: 'num',
+          },
+          { text: '제목', value: 'title' },
+          { text: '작성자', value: 'writer' },
+          { text: '작성일시', value: 'writedate' },
+        ],
+        desserts: [
+          {
+            num: 1,
+            title: "게시글1",
+            writer: "작성자1",
+            writedate: "2023-03-05 03:13",
+          },
+        ],
+      }
+    },
+  }
 </script>
